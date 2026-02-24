d8bda0efe94c73b9d62f10a019243316fed68b7d
Valeev Serj <145398532+Gabryelf@users.noreply.github.com>
2026-02-25 00:02:35 +0300

    Update players.sql

--- teams/red/players.sql
+++ teams/red/players.sql
@@ -1,1 +1,9 @@
-
+CREATE TABLE Players (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    nickname TEXT NOT NULL,
+    level INTEGER CHECK(level BETWEEN 1 AND 14),
+    experience INTEGER DEFAULT 0,
+    arena TEXT DEFAULT 'Arena 1',
+    clan_id INTEGER,
+    FOREIGN KEY (clan_id) REFERENCES Clans(id)
+);
