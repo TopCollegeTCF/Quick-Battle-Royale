f9f60c15d2663341a078d3eebc2177c6dc8c2772
Billi nogamy <tuyyyy45yuuuu@gmail.com>
2026-02-25 17:49:04 +0300

    Update battles.sql

--- teams/blue/battles.sql
+++ teams/blue/battles.sql
@@ -8,6 +8,3 @@ CREATE TABLE battles (
     FOREIGN KEY (player2_id) REFERENCES players(id),
     FOREIGN KEY (winner_id) REFERENCES players(id)
 );
-INSERT INTO battles (player1_id, player2_id, winner_id)
-VALUES (1,2,1);CREATE TABLE IF NOT EXISTS users(
-  id INTEGER PRIMARY KEY AUTOINCREMENT,
