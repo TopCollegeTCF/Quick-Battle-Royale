2458e3258ba1a18548c99098dd6d6f7f572923c9
SuperDedСемён1942 <semen52.crow@mail.ru>
2026-02-25 18:56:28 +0300

    Update player_cards.sql

--- teams/red/player_cards.sql
+++ teams/red/player_cards.sql
@@ -1,1 +1,17 @@
+CREATE TABLE IF NOT EXISTS player_cards (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    user_id INTEGER,
+    username TEXT,
+    card_name TEXT,
+    rarity TEXT
+);
 
+INSERT INTO player_cards (user_id, username, card_name, rarity) VALUES 
+(1, 'ClashMaster', 'Мегарыцарь', 'Легендарная'),
+(1, 'ClashMaster', 'Всадник на кабане', 'Редкая'),
+(2, 'Сёма_Чебурек', 'Принц', 'Эпическая'),
+(2, 'Сёма_Чебурек', 'Рыцарь', 'Обычная'),
+(3, 'King_CR', 'Спарки', 'Легендарная'),
+(3, 'King_CR', 'Мушкетер', 'Редкая');
+
+SELECT * FROM player_cards;
