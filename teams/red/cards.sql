de70995695620a84cd5bdc1d1d346e6f64922d00
Erlan <erlanusmonov47@gmail.com>
2026-02-25 17:53:57 +0300

    Update cards.sql (Erlan)

--- teams/red/cards.sql
+++ teams/red/cards.sql
@@ -1,1 +1,13 @@
-
+CREATE TABLE cards (
+  	id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name VARCHAR(30) NOT NULL,
+  	is_legends BOOLEAN DEFAULT 0,
+	  costOfTheElixir INTEGER NOT NULL,
+  	damagePerSecond INTEGER NOT NULL,
+  	health INTEGER NOT NULL,
+  	movementSpeed INTEGER NOT NULL,
+  	rangeAttack INTEGER NOT NULL,
+  	targetEarthOrAir INTEGER NOT NULL,
+  	fromWhichArena INTEGER NOT NULL,
+  	imageURL VARCHAR NOT NULL
+);
