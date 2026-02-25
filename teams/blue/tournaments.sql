a4dd5e520cecebd8a2dc8b8d24f4a610c08bc51a
prosto73 <prosto209999@mail.ru>
2026-02-25 18:48:40 +0300

    Update tournaments.sql

--- teams/blue/tournaments.sql
+++ teams/blue/tournaments.sql
@@ -1,1 +1,6 @@
-
+CREATE TABLE IF NOT EXISTS tournaments (
+  THE_PRIZE_BACKGROUND varchar(50) not NULL,
+  NUMBER_OF_CLANS varchar(50) not NULL,
+  CLANS_WINER varchar(50) not NULL,
+  DATE_OF_THE_EVENT varchar(50) not NULL
+);
