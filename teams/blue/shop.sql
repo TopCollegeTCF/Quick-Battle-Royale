2799a1b334b7c0eebaa6e950fc6b447b76470237
nebowork <carharttovich@gmail.com>
2026-02-25 18:52:41 +0300

    Update shop.sql

--- teams/blue/shop.sql
+++ teams/blue/shop.sql
@@ -1,1 +1,9 @@
+CREATE TABLE if not EXISTS shop(
+	gem_price_card varchar(50) not NULL,
+	gem_price_money varchar(50) not NULL,
+  	gem_price_evolution varchar(50) not NULL,
+  	gem_price_hero varchar(50) not NULL,
+  	gems_price varchar(50) NOT NULL,
+  	gem_price_emotion varchar(50) not NULL
+);
 
