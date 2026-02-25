0d475751867b1af85eecb0eec33cc3c06c344e5b
Billi nogamy <tuyyyy45yuuuu@gmail.com>
2026-02-25 17:25:32 +0300

    Update battles.sql

--- teams/blue/battles.sql
+++ teams/blue/battles.sql
@@ -1,1 +1,10 @@
-
+CREATE TABLE battles (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    player1_id INTEGER NOT NULL,
+    player2_id INTEGER NOT NULL,
+    winner_id INTEGER,
+    battle_date DATETIME DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (player1_id) REFERENCES players(id),
+    FOREIGN KEY (player2_id) REFERENCES players(id),
+    FOREIGN KEY (winner_id) REFERENCES players(id)
+);
