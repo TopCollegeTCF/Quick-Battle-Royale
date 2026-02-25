3a49b70bb134a4b3bf36f4b4822f59a3c32295de
Billi nogamy <tuyyyy45yuuuu@gmail.com>
2026-02-25 17:48:37 +0300

    Update shop.sql

--- teams/blue/shop.sql
+++ teams/blue/shop.sql
@@ -1,1 +1,10 @@
-
+CREATE TABLE shop (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    item_name TEXT,"Legendary Chest"
+    item_type TEXT, "chest"
+    rarity TEXT,"легендарный"
+    price_gems INTEGER,
+    price_gold INTEGER,
+);
+INSERT INTO shop (item_name, item_type, price_gems, available_until) 
+VALUES ('Legendary Chest', 'chest', 500, 10000,);
