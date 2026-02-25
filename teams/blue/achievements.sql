5e50f95847d1d0d5455298e83418c530a2a20600
Valeev Serj <145398532+Gabryelf@users.noreply.github.com>
2026-02-25 12:35:54 +0300

    Update achievements.sql

--- teams/blue/achievements.sql
+++ teams/blue/achievements.sql
@@ -1,1 +1,9 @@
-
+CREATE TABLE achievements (
+  id INTEGER AUTOINCREMENT,
+  imageid INTEGER,
+  name TEXT NOT NULL,
+  description TEXT NOT NULL,
+  whengot TIME,
+  rare VARCHAR,
+  expreward FLOAT DEFAULT 0.0
+);
