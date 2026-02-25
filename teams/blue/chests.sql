f764ab9bee05f7263841632bc0b39812bd440a5a
Billi nogamy <tuyyyy45yuuuu@gmail.com>
2026-02-25 17:40:12 +0300

    Update chests.sql

--- teams/blue/chests.sql
+++ teams/blue/chests.sql
@@ -1,1 +1,10 @@
-
+CREATE TABLE chests (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    player_id INTEGER,
+    chest_type TEXT, -- например: 'Silver', 'Golden', 'Magical', 'Legendary'
+    arena TEXT,      -- арена, на которой получен сундук
+    is_opened INTEGER DEFAULT 0, -- 0 - закрыт, 1 - открыт
+    received_date DATETIME DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (player_id) REFERENCES players(id)
+);
+INSERT INTO chests (player_id, chest_type, arena) VALUES (1, 'Silver', 'Arena 4');
