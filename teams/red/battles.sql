fa9466bf63dbd9b39a56b766a8852ba7ccdeb7a5
Valeev Serj <145398532+Gabryelf@users.noreply.github.com>
2026-02-25 12:25:44 +0300

    Update battles.sql

--- teams/red/battles.sql
+++ teams/red/battles.sql
@@ -1,1 +1,12 @@
-
+CREATE TABLE battles (
+	  elixir_start INTEGER DEFAULT 0,
+  	elixir_refil_amount INTEGER DEFAULT 1,
+	  elixir_refil_cooldown INTEGER,
+ 	  elixir_card_cost INTEGER,
+    cards_amount INTEGER,
+  	match_start_time TIME DEFAULT "03:00",
+  	match_spend_time INTEGER DEFAULT 1,
+  	tower_noking_hp INTEGER DEFAULT 2500,
+  	tower_withking_hp INTEGER DEFAULT 5000,
+  	amount_of_towers INTEGER	
+);
