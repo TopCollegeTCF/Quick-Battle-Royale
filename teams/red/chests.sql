616f5b764143b4c59612011e3ecc7cd13582055a
krossovkaEncore <krossovka160609@gmail.com>
2026-02-25 17:56:21 +0300

    1

--- teams/red/chests.sql
+++ teams/red/chests.sql
@@ -1,1 +1,7 @@
-
+CREATE TABLE chests (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT NOT NULL,
+  	rarity TEXT CHECK in ("Normal", "Rare", "Legendary", "Super legendary", "Super puper pro max legendary"),
+  	price INTEGER,
+    max_quantity INTEGER DEFAULT 0
+);
