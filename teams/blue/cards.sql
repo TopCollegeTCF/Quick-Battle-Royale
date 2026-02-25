195261c73271176c4cfcf5c0a0d1a84c76fceb51
Billi nogamy <tuyyyy45yuuuu@gmail.com>
2026-02-25 17:23:18 +0300

    Update cards.sql

--- teams/blue/cards.sql
+++ teams/blue/cards.sql
@@ -2,4 +2,4 @@ CREATE TABLE cards (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     name TEXT NOT NULL,
     rarity TEXT NOT NULL,        -- common, rare, epic, legendary
-    elixir_cost INTEGER NOT NULL
+    elixir_cost INTEGER NOT NULL);
