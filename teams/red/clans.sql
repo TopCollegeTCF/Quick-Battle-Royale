0b254d0cfd5cb68a35485af9f6b0a5d65290570e
Valeev Serj <145398532+Gabryelf@users.noreply.github.com>
2026-02-25 16:29:06 +0300

    Update clans.sql

--- teams/red/clans.sql
+++ teams/red/clans.sql
@@ -2,5 +2,12 @@ CREATE TABLE Clans (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     name TEXT NOT NULL,
     trophies INTEGER DEFAULT 0,
-    max_members INTEGER DEFAULT 50
+    max_members INTEGER DEFAULT 50,
+  	data_create TIME,
+  	level INTEGER DEFAULT 0,
+  	player_members INTEGER NOT NULL,
+  	num_matches INTEGER DEFAULT 0,
+  	num_losses INTEGER DEFAULT 0,
+  	num_win INTEGER DEFAULT 0
+    
 );
