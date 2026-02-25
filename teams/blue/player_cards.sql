bfae4ea734558233343380e6bb6e8f1da140355e
Valeev Serj <145398532+Gabryelf@users.noreply.github.com>
2026-02-25 12:51:25 +0300

    Update player_cards.sql

--- teams/blue/player_cards.sql
+++ teams/blue/player_cards.sql
@@ -1,1 +1,8 @@
-
+CREATE TABLE player_cards(
+	id INTEGER PRIMARY KEY,
+  	name VARCHAR NOT NULL ,
+  	mana_cost INTEGER DEFAULT 50,
+ 	class VARCHAR NOT NULL,
+  	type VARCHAR NOT NULL,
+  	player_id INTEGER
+);
